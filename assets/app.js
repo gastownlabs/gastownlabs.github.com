21cf703989efebe8e34830adef648ced3e749e91
Adam Maddox <adam@adammaddox.com>
2012-07-20 09:00:44 -0700

    update

--- assets/app.js
+++ assets/app.js
@@ -5,10 +5,38 @@
     scrollorama = $.scrollorama({
       blocks: "section"
     });
-    return scrollorama.animate("h3", {
-      duartion: 600,
-      property: "padding-top",
-      pin: true
+    scrollorama.onBlockChange(function() {
+      if (scrollorama.blockIndex === 1) {
+        return $('header').show();
+      }
+    });
+    scrollorama.animate("section img#logo", {
+      delay: 10,
+      duration: 100,
+      property: 'width',
+      start: 792,
+      end: 500
+    });
+    scrollorama.animate("li.step#strategy", {
+      delay: 200,
+      duration: 200,
+      property: 'zoom',
+      start: 1,
+      end: 0
+    });
+    scrollorama.animate("li.step#design", {
+      delay: 150,
+      duration: 150,
+      property: 'zoom',
+      start: 1,
+      end: 0
+    });
+    return scrollorama.animate("li.step#build", {
+      delay: 100,
+      duration: 100,
+      property: 'zoom',
+      start: 1,
+      end: 0
     });
   });
 
